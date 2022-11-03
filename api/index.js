5ea6cabd8dd0fe34d6f199ba4d3542185822842f
unknown <tomygarnero9429@gmail.com>
2022-11-03 13:54:09 -0300

    Change Port

--- api/index.js
+++ api/index.js
@@ -22,7 +22,7 @@ const { conn } = require('./src/db.js');
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {  // true se carga siempre // false se carga una vez
-  server.listen(3001, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+  server.listen(`${PORT}`, () => {
+    console.log(`%s listening at ${PORT}`); // eslint-disable-line no-console
   });
 });
