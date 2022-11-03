c260a09ea54212d3c6c51a0c9c2b3f9c0a2b9aa2
unknown <tomygarnero9429@gmail.com>
2022-11-03 14:42:12 -0300

    Production

--- api/index.js
+++ api/index.js
@@ -22,7 +22,7 @@ const { conn } = require('./src/db.js');
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {  // true se carga siempre // false se carga una vez
-  server.listen(`${PORT}`, () => {
+  server.listen(process.env.PORT, () => {
     console.log(`%s listening at ${PORT}`); // eslint-disable-line no-console
   });
 });
